ee8df00d2d1922e8d73e55ea133d3e87743b761f
Mahak Preet kaur <mahakpreet65@gmail.com>
2024-12-26 00:04:38 +0530

    Add files via upload

---
+++ variables.R
@@ -0,0 +1,27 @@
+
+
+
+#integer
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.4
+typeof(y)
+
+#complex
+z <- 4+3i
+typeof(z)
+
+
+#character
+a <- "H"
+typeof(a)
+
+
+
+#logical
+q1 <- T
+typeof(q1)
+q2 <- F
+typeof(q2)
